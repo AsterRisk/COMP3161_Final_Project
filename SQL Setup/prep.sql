9feecddcdeec7b7f48e5d6cb7f23ce29ea4ec480
AsterRisk <31030090+AsterRisk@users.noreply.github.com>
2020-05-10 00:30:16 -0500

    Add files via upload

--- SQL Setup/prep.sql
+++ SQL Setup/prep.sql
@@ -65,21 +65,12 @@ create table posts
     primary key (post_id)
 );
 
-
-create table media
-(
-    user_id int not null,
-    media_link varchar(70) not null unique,
-    primary key (media_link)
-);
-
 create table comments
 (
     comment_id int not null UNIQUE,
     user_id int not null,
     post_id int not null,
     text_content varchar (300),
-    media_link varchar(30),
     primary key (comment_id)
 );
 
@@ -98,3 +89,85 @@ create table members
 	role varchar (15) not null,
     primary key (group_id, member_id)
 );
+
+create table friend_requests
+(
+	requester_id int not null,
+	requestee_id int not null,
+	primary key (requester_id, requestee_id)
+);
+
+create table group_posts
+(
+	g_post_id int not null UNIQUE,
+    user_id int not null,
+	group_id int not null,
+    text_content varchar(300),
+    media_link varchar(70),
+    /*foreign key (user_id) references users,*/
+    primary key (g_post_id)
+);
+
+create table group_comments
+(
+	g_comment_id int not null UNIQUE,
+    member_id int not null,
+    g_post_id int not null,
+    text_content varchar (300),
+    primary key (g_comment_id)
+);
+
+delimiter //
+create procedure acceptFriendRequest(IN requesting int, requested int)
+begin
+	delete from friend_requests where requester_id = requesting and requestee_id = requested;
+	insert into friends (user_id, friend_id, fgroup) values (requesting, requested, "Default");
+	insert into friends (user_id, friend_id, fgroup) values (requested, requesting, "Default");
+end;
+
+create procedure deleteFriendRequest(IN requesting int, requested int)
+begin
+	delete from friend_requests where requester_id = requesting and requestee_id = requested;
+end;
+
+create procedure joinGroup(IN newMemberId int, groupID int)
+begin 
+	insert into members (group_id, member_id, role) values (groupID, newMemberId, "Member");
+end;
+
+create procedure createGroup (IN gname varchar(30), creator_id int, dp_address varchar(70))
+begin
+    DECLARE numGroups INT;
+    select count(group_id) into numGroups from groups;
+    insert into groups (group_id, owner_id, group_name, group_dp_location) values (numGroups+1, creator_id, gname, dp_address);
+    insert into members (group_id, member_id, role) values (numGroups+1, creator_id, "Owner");
+end;
+
+create procedure searchUser(IN searchVal varchar(30))
+begin
+	select * from users where first_name like searchVal or last_name like searchVal or concat(first_name, concat(" ", last_name)) like searchVal;
+end;
+
+create procedure searchGroup(IN searchVal varchar(30))
+begin
+	select * from groups where group_name like searchVal;
+end;
+
+create procedure requestFriend(IN requesting int, requested int)
+begin
+	insert into friend_requests (requester_id, requestee_id) values (requesting, requested);
+end;
+
+create procedure promoteUser(IN g_id int, m_id int)
+begin
+	update members set role = "Content Editor" where member_id = m_id and group_id = g_id;
+end;
+
+create procedure leaveGroup(IN g_id int, m_id int)
+begin
+	delete from members where group_id = g_id and member_id = m_id;
+end;
+	
+//
+
+delimiter ;
